5d047f4244c87765a7295c1b67fcbd14009353b6
erinlizzierose <erin.lizzie.rose@gmail.com>
2022-04-16 15:41:24 +1200

    add password

---
+++ backup-downloader.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# required b2 client from Backblaze and 7z CLI tool to be installed 
+
+# ask for password 
+echo -n "Enter a password: "
+read -s password 
+# log into backblaze
+# make sure 7z is installed
+# unpack archive
+
+
+
